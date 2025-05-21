bc49ec8eeace8e1bc195ec142bb80283740bd8ea
Mithat <mithatsirmali@gmail.com>
2025-05-21 14:59:00 +0300

    Add files via upload

---
+++ Erişim Belirleyiciler1/Erişim Belirleyiciler1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erişim_Belirleyiciler1
+{
+    internal class Program
+    {
+        class Kisi
+        {
+            public string Ad;              // Her yerden erişilebilir
+            private string TCKimlikNo;     // Sadece sınıf içinden erişilebilir
+            protected int Yas;             // Türeyen sınıflardan erişilebilir
+            internal string Sehir;         // Aynı projedeki her sınıftan erişilebilir
+
+            public Kisi(string ad, string tc, int yas, string sehir)
+            {
+                Ad = ad;
+                TCKimlikNo = tc;
+                Yas = yas;
+                Sehir = sehir;
+            }
+
+            public void BilgileriYazdir()
+            {
+                Console.WriteLine($"Ad: {Ad}, TC: {TCKimlikNo}, Yaş: {Yas}, Şehir: {Sehir}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Kisi k1 = new Kisi("Ayşe", "12345678901", 30, "İstanbul");
+            k1.BilgileriYazdir();
+
+            Console.WriteLine($"Ad: {k1.Ad}");
+            Console.WriteLine($"Şehir: {k1.Sehir}");
+
+            // k1.TCKimlikNo → private olduğu için erişilemez
+            // k1.Yas → protected olduğu için burada erişilemez
+        }
+    }
+}
---
+++ Erişim Belirleyiciler1/Erişim Belirleyiciler1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Erişim Belirleyiciler1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Erişim Belirleyiciler1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0b1662ab-2ca8-431c-8d02-eddad459ed2e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
